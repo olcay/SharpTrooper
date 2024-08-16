6f900fb6124fe680c7e66fb7c5afaedd04b59773
Matt Fricker <matt@fricker.io>
2024-08-16 22:10:37 +0100

    use .net upgrade assistant to migrate to .net 8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SharpTrooper")]
-[assembly: AssemblyDescription("A C# helper library for swapi.co - the Star Wars API")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Olcay Bayram")]
-[assembly: AssemblyProduct("SharpTrooper")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0e841ed9-aa91-49cc-bc26-be99ae6a194d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
