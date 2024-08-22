119b1b58ce4fcedbe2d96e85a8a716cfcdc80c6d
Matt Fricker <matt@fricker.io>
2024-08-22 13:44:30 +0100

    add example project to show implementation

---
+++ SharpTrooper.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SharpTrooper.Core;
+
+using (var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHttpClient();
+        services.AddScoped<SharpTrooperCore>();
+    })
+    .Build())
+{
+    using var scope = host.Services.CreateScope();
+    var sharpTrooperService = scope.ServiceProvider.GetService<SharpTrooperCore>()
+        ?? throw new NullReferenceException("service is null");
+
+    var films = await sharpTrooperService.GetAllFilmsAsync();
+
+    Console.WriteLine("List of films from SWAPI:");
+
+    foreach (var item in films.results)
+    {
+        Console.WriteLine(item.title);
+    }
+}
