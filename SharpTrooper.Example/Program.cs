5d4bd2d76424a3c46c49f31bf4bcd83307446a31
Matt Fricker <matt@fricker.io>
2024-10-23 21:13:17 +0100

    clear default logging + add lists of data

--- SharpTrooper.Example/Program.cs
+++ SharpTrooper.Example/Program.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SharpTrooper.Core;
 
 using (var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+    })
     .ConfigureServices((context, services) =>
     {
         services.AddHttpClient();
@@ -14,12 +19,52 @@ using (var host = Host.CreateDefaultBuilder(args)
     var sharpTrooperService = scope.ServiceProvider.GetService<SharpTrooperCore>()
         ?? throw new NullReferenceException("service is null");
 
-    var films = await sharpTrooperService.GetAllFilmsAsync();
+    Console.WriteLine("SWAPI:");
 
-    Console.WriteLine("List of films from SWAPI:");
+    Console.WriteLine("...");
+
+    var films = await sharpTrooperService.GetAllFilmsAsync();
 
     foreach (var item in films.results)
     {
         Console.WriteLine(item.title);
     }
+
+    Console.WriteLine("...");
+
+    var people = await sharpTrooperService.GetAllPeopleAsync();
+
+    foreach (var item in people.results)
+    {
+        Console.WriteLine(item.name);
+    }
+
+    Console.WriteLine("...");
+
+    var vehicles = await sharpTrooperService.GetAllVehiclesAsync();
+
+    foreach (var item in vehicles.results)
+    {
+        Console.WriteLine(item.name);
+    }
+
+    Console.WriteLine("...");
+
+    var species = await sharpTrooperService.GetAllSpeciesAsync();
+
+    foreach (var item in species.results)
+    {
+        Console.WriteLine(item.name);
+    }
+
+    Console.WriteLine("...");
+
+    var planets = await sharpTrooperService.GetAllPlanetsAsync();
+
+    foreach (var item in planets.results)
+    {
+        Console.WriteLine(item.name);
+    }
+
+    Console.WriteLine("...");
 }
